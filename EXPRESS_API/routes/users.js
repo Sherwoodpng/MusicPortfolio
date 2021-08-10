5838f0e8b8b2582d7db842e1d6b761865d36c405
Zachary Sherwood <zachsherwood@boisestate.edu>
2021-08-10 10:07:57 -0600

    testing json

--- EXPRESS_API/routes/users.js
+++ EXPRESS_API/routes/users.js
@@ -2,8 +2,19 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-  res.send('Hello');
+const users = [
+  {
+    firstName: "Zach",
+    lastName: "Sherwood",
+    age: 32
+  }
+]
+
+//all routes in here are starting with /users
+router.get('/', (req, res) => {
+  console.log(users);
+  console.log("test");
+  res.send('Hellosdfsdfsdf');
 }); 
 
 export default router;
